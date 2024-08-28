83d8ab6182aba8552302620a685776c1c3b8b4e7
mischanvp <mischan.vali@gmail.com>
2024-08-28 16:15:52 +0200

    merge nav to header and refine stylining

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import Header from  './components/Header'
-import Nav from  './components/Nav'
 import Main from  './components/Main'
 import Footer from  './components/Footer'
 
 function App() {
   return (
     <div className="site-container">
-      <header className="nav-header">
-        {/* Header section */}
-        <Header />
-        {/* Navigation section */}
-        <Nav />
-      </header>
+      {/* Header and navigation section */}
+      <Header />
       {/* Main section */}
       <Main />
       {/* Footer section */}
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import Logo from '../pics/Logo.svg'
-import '../style.css'
+import '../style.css';
+import Logo from '../pics/Logo.svg';
 
 function Header() {
     return (
-    <img src={Logo} alt="Little Lemon Logo" className="pic-logo" />
-    )
+        <header>
+            <nav className="nav-header">
+                <img src={Logo} alt="Little Lemon Logo" className="pic-logo" />
+                <ul className="nav-list">
+                    <li><a href="#home">Home</a></li>
+                    <li className="nav-item-hidden"><a href="#About">About</a></li>
+                    <li className="nav-item-hidden"><a href="#Menu">Menu</a></li>
+                    <li><a href="#Reservations">Reservations</a></li>
+                    <li><a href="#Order">Order</a></li>
+                    <li className="nav-item-hidden"><a href="#Login">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
