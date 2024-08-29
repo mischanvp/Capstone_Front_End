6012ffee266bf454072316c658cce25bab79a431
mischanvp <mischan.vali@gmail.com>
2024-08-29 16:07:39 +0200

    include burger menu on phone

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,19 +1,29 @@
 import '../style.css';
 import Logo from '../pics/Logo.svg';
+import BurgerIcon from '../pics/burgericon.svg';
 import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
 
 function Header() {
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
     return (
         <header>
             <nav className="nav-header">
+                <img src={BurgerIcon} alt="Menu" className="burger-icon" onClick={toggleNav} />
                 <img src={Logo} alt="Little Lemon Logo" className="pic-logo" />
-                <ul className="nav-list">
+                <ul className={`nav-list ${isNavOpen ? "open" : ""}`}>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/" className="nav-item-hidden" >About</Link></li>
-                    <li><Link to="/" className="nav-item-hidden" >Menu</Link></li>
-                    <li><Link to="/reserve">Reservations</Link></li>
+                    <li><Link to="/">About</Link></li>
+                    <li><Link to="/">Menu</Link></li>
+                    <li><Link to="/reserve" onClick={toggleNav}>Reservations</Link></li>
                     <li><Link to="/">Order Online</Link></li>
-                    <li><Link to="/" className="nav-item-hidden" >Login</Link></li>
+                    <li><Link to="/">Login</Link></li>
                 </ul>
             </nav>
         </header>
